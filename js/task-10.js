fecae7bdd9a00b96f8d1b7532e6309285468f0c4
Святослав Минчаков <sminchakov@gmail.com>
2023-07-01 19:59:37 +0300

    added 10-th task

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const enterNumber = document.querySelector('input');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+const boxesContainer = document.querySelector('#boxes');
+
+
+
+btnCreate.addEventListener('click', isCreateBoxes);
+btnDestroy.addEventListener('click', isDestroyBoxes);
+
+function isCreateBoxes() {
+  let counter = enterNumber.value
+  let areaCounter = 20;
+
+  for(let i = 0; i <  counter; i++) {
+    areaCounter +=10;
+    const newBox = document.createElement('div');
+    newBox.style.backgroundColor = getRandomHexColor();
+    newBox.style.width = `${areaCounter}px`;
+    newBox.style.height = `${areaCounter}px`;
+    boxesContainer.appendChild(newBox);
+  }
+  
+}
+
+function isDestroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
